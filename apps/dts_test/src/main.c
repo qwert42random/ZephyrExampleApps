1c91ec67522263955583e5d3b3b7ec199f3ca7d1
Christopher Chung <Cskywalker1@gmail.com>
2026-07-15 14:16:15 +0100

    Feat: working dts example

--- apps/dts_test/src/main.c
+++ apps/dts_test/src/main.c
@@ -8,39 +8,21 @@
 struct test_config first_test_config = EXAMPLE_DT_GET_TEST_BY_IDX(test1);
 struct test_config second_test_config = EXAMPLE_DT_GET_TEST_BY_IDX(test2);
 
-int main2(void) {
-    while(1) {
-        int ret = test_print();
-        printf("Return value: %d\n", ret);
-        printf("test_func (first config): %d\n", test_func(first_test_config));
-        printf("test_func (second config): %d\n", test_func(second_test_config));
-
-#if DT_HAS_COMPAT_STATUS_OKAY(custom_compatible)
-        printf("Hello there, world\n");
-#endif
-
-        k_msleep(1000);
-    }
-}
-
 struct Gpio green = {
-    .reg = D,
+    .reg = DT_STRING_TOKEN(DT_PATH(test, green_light), gpio_port),
     .pin = DT_PROP(DT_PATH(test, green_light), pin),
-    .pull = pull_down,
-
-    .mode = output
+    .pull = DT_STRING_TOKEN(DT_PATH(test, green_light), pull_type),
+    .mode = DT_STRING_TOKEN(DT_PATH(test, green_light), output)
 };
 
 struct Gpio usr_button = {
-    .reg = A,
+    .reg = DT_STRING_TOKEN(DT_PATH(test, button), gpio_port),
     .pin = DT_PROP(DT_PATH(test, button), pin),
-    .pull = pull_down,
-
-    .mode = input
+    .pull = DT_STRING_TOKEN(DT_PATH(test, button), pull_type),
+    .mode = DT_STRING_TOKEN(DT_PATH(test, button), output)
 };
 
 int main(void) {
-
     init_gpio(green);
     init_gpio(usr_button);
 
@@ -48,10 +30,19 @@ int main(void) {
     write_pin(green, pin_state);
 
     while(1) {
+
         if (read_pin(usr_button)) {
-            printf("Hello world\n");
+            test_print();
+
             pin_state = !pin_state;
             write_pin(green, pin_state);
+
+            printf("First test config: %d\n", test_func(first_test_config));
+            printf("Second test config: %d\n", test_func(second_test_config));
+
+            if (DT_HAS_COMPAT_STATUS_OKAY(custom_compatible)) {
+                printf("custom,compatible in device tree\n");
+            }
 
             k_msleep(300);
             while (read_pin(usr_button));
